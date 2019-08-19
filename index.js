e063076cee27ab185491df47d847f1af97bdbf0f
ibtarek <ibtarek@gmail.com>
2019-08-19 10:07:20 -0700

    Initial import

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// predefined constants
+const TEMPLATE_PATH = "./templates";
+
+const httpRequest = require("request-promise-native");
+const bunyan = require("bunyan");
+const fs = require("fs");
+const HumanApiClient = require("./lib/humanapi-client");
+const transform = require("./lib/transform");
+const render = require("./lib/render-handlebars")(TEMPLATE_PATH);
+const load = require("./lib/load");
+const program = require('commander');
+const pkg = require("./package.json");
+
+
+
+const log = bunyan.createLogger({
+  name : "hapi-client",
+  level : "debug"
+})
+
+const client = new HumanApiClient({
+  httpRequest,
+  log : log.child({ client : "1"}),
+  baseUrl : "https://api.humanapi.co/v1/human"
+})
+
+
+
+program
+  .version(pkg.version)
+  .option('-t, --token <token>', 'Human API access token')
+  .option('-o, --out <filename>', 'small pizza size');
+
+program.parse(process.argv);
+
+load(client,program.token)
+.then(feed=>transform(client,program.token,feed))
+.then(render)
+.then(html=>fs.writeFileSync(program.out,html));
